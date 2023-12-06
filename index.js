c1be8e1f7c7b005f20bc06d0f770b96fe0ed312f
Adjel van der Meer <eavdmeer@gmail.com>
2023-12-06 12:41:06 +0100

    Run day 5

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import day01 from './day01/index.js';
 import day02 from './day02/index.js';
 import day03 from './day03/index.js';
 import day04 from './day04/index.js';
-/*
 import day05 from './day05/index.js';
+/*
 import day06 from './day06/index.js';
 import day07 from './day07/index.js';
 import day08 from './day08/index.js';
@@ -39,8 +39,8 @@ async function getResults()
     await day02('day02/data.txt').then(report),
     await day03('day03/data.txt').then(report),
     await day04('day04/data.txt').then(report),
-    /*
     await day05('day05/data.txt').then(report),
+    /*
     await day06('day06/data.txt').then(report),
     await day07('day07/data.txt').then(report),
     await day08('day08/data.txt').then(report),
