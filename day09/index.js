d940a4ac4f709e8e9c6cc0d3265b170da9b244d1
Adjel van der Meer <eavdmeer@gmail.com>
2023-12-09 12:04:06 +0100

    Add day 9 solutions

---
+++ day09/index.js
@@ -0,0 +1,102 @@
+import * as fs from 'fs/promises';
+import makeDebug from 'debug';
+
+const debug = makeDebug('day09');
+
+if (process.argv[2])
+{
+  day09(process.argv[2]).then(console.log);
+}
+
+function evolve(rows)
+{
+  const row = rows[rows.length - 1];
+
+  if (row.every(v => v === 0)) { return rows; }
+
+  rows.push(row.map((v, i, m) => i === 0 ? 0 : v - m[i - 1]).slice(1));
+
+  return evolve(rows);
+}
+
+function predict(grid)
+{
+  return grid
+    .map(row => row[row.length - 1])
+    .reduce((a, v) => a + v, 0);
+}
+
+function solve1(data)
+{
+  const grid = data
+    .map(v => v.split(' ').map(w => parseInt(w, 10)));
+
+  const matrix = grid.map(v => evolve([ v ]));
+
+  const predictions = matrix.map(v => predict(v));
+
+  return predictions.reduce((a, v) => a + v, 0);
+}
+
+function predict2(grid)
+{
+  return [ ...grid ].reverse()
+    .reduce((a, v) => v[0] - a, 0);
+  /*
+  let last = 0;
+  return [ ...grid ].reverse()
+    .map(row =>
+    {
+      const n = row[0] - last;
+      last = n;
+      return n;
+    })
+    .pop();
+    */
+}
+
+function solve2(data)
+{
+  const grid = data
+    .map(v => v.split(' ').map(w => parseInt(w, 10)));
+
+  const matrix = grid.map(v => evolve([ v ]));
+
+  const predictions = matrix.map(v => predict2(v));
+
+  return predictions.reduce((a, v) => a + v, 0);
+}
+
+export default async function day09(target)
+{
+  const start = Date.now();
+  debug('starting');
+
+  const buffer = await fs.readFile(target);
+
+  /* eslint-disable no-shadow */
+  const data = buffer
+    .toString()
+    .trim()
+    .split(/\s*\n\s*/)
+    .filter(v => v);
+  /* eslint-enable no-shadow */
+
+  debug('data', data);
+
+  const part1 = solve1(data);
+  const expect1 = 114;
+  if (target.includes('example') && part1 !== expect1)
+  {
+    throw new Error(`Invalid part 1 solution: ${part1}. Expecting; ${expect1}`);
+  }
+
+  const part2 = solve2(data);
+  const expect2 = 2;
+  if (target.includes('example') && part2 !== expect2)
+  {
+    throw new Error(`Invalid part 2 solution: ${part2}. Expecting; ${expect2}`);
+  }
+
+  return { day: 'day09', part1, part2, duration: Date.now() - start };
+}
--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import day05 from './day05/index.js';
 import day06 from './day06/index.js';
 import day07 from './day07/index.js';
 import day08 from './day08/index.js';
-/*
 import day09 from './day09/index.js';
+/*
 import day10 from './day10/index.js';
 import day11 from './day11/index.js';
 import day12 from './day12/index.js';
@@ -43,8 +43,8 @@ async function getResults()
     await day06('day06/data.txt').then(report),
     await day07('day07/data.txt').then(report),
     await day08('day08/data.txt').then(report),
-    /*
     await day09('day09/data.txt').then(report),
+    /*
     await day10('day10/data.txt').then(report),
     await day11('day11/data.txt').then(report),
     await day12('day12/data.txt').then(report),
