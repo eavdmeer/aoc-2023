18d5d2092b8fc2596478b8ede5542842e81f8373
Adjel van der Meer <eavdmeer@gmail.com>
2023-12-16 14:57:04 +0100

    Add part 2 solution

--- day16/index.js
+++ day16/index.js
@@ -1,6 +1,8 @@
 import * as fs from 'fs/promises';
 import makeDebug from 'debug';
 
+/* eslint-disable no-else-return */
+
 const debug = makeDebug('day16');
 
 if (process.argv[2])
@@ -8,153 +10,115 @@ if (process.argv[2])
   day16(process.argv[2]).then(console.log);
 }
 
-function walk(grid, pos, dir, visited, beam = 0)
+const offGrid = (grid, r, c) => r < 0 || r >= grid.length ||
+  c < 0 || c >= grid[0].length;
+
+function walk(grid, r, c, dir, visited = new Set(), m = new Set(), beam = 0)
 {
-  debug('beam:', beam, 'pos:', pos, 'dir:', dir);
+  debug('beam:', beam, 'pos:', r, c, 'dir:', dir);
   debug(visited.size);
 
-  const key = `${pos.r}-${pos.c}:${dir.r}-${dir.c}}`;
+  const key = `${r}-${c}:${dir.r}-${dir.c}}`;
 
   if (visited.has(key))
   {
     debug('circular path detected, terminating beam', beam);
-    return;
+    return m.size;
   }
 
-  if (pos.c >= 0)
+  if (! offGrid(grid, r, c))
   {
     visited.add(key);
+    m.add(`${r},${c}`);
   }
 
-  const npos = { r: pos.r + dir.r, c: pos.c + dir.c };
+  const npos = { r: r + dir.r, c: c + dir.c };
 
   // detect going off grid
-  if (npos.r < 0 || npos.r >= grid.length ||
-    npos.c < 0 || npos.c >= grid[0].length)
+  if (offGrid(grid, npos.r, npos.c))
   {
     debug('going off grid, terminating beam', beam);
-    return;
+    return m.size;
   }
 
   const ch = grid[npos.r].charAt(npos.c);
 
   if (ch === '.')
   {
-    debug('empty spot, walking on');
-    walk(grid, npos, dir, visited, beam);
+    return walk(grid, npos.r, npos.c, dir, visited, m, beam);
   }
-  else if (ch === '-')
+
+  if (ch === '-')
   {
     if (dir.r === 0)
     {
-      debug('walk through horizontally');
-      walk(grid, npos, dir, visited, beam);
+      return walk(grid, npos.r, npos.c, dir, visited, m, beam);
     }
-    else
-    {
-      debug('split beam horizontally');
-      walk(grid, npos, { r: 0, c: -1 }, visited, beam);
-      walk(grid, npos, { r: 0, c: 1 }, visited, beam + 1);
-    }
+
+    return Math.max(
+      walk(grid, npos.r, npos.c, { r: 0, c: -1 }, visited, m, beam),
+      walk(grid, npos.r, npos.c, { r: 0, c: 1 }, visited, m, beam + 1)
+    );
   }
-  else if (ch === '|')
+
+  if (ch === '|')
   {
     if (dir.c === 0)
     {
-      debug('walk through vertically');
-      walk(grid, npos, dir, visited);
-    }
-    else
-    {
-      debug('split beam vertically');
-      walk(grid, npos, { r: -1, c: 0 }, visited, beam);
-      walk(grid, npos, { r: 1, c: 0 }, visited, beam + 1);
+      return walk(grid, npos.r, npos.c, dir, visited, m, beam);
     }
 
+    return Math.max(
+      walk(grid, npos.r, npos.c, { r: -1, c: 0 }, visited, m, beam),
+      walk(grid, npos.r, npos.c, { r: 1, c: 0 }, visited, m, beam + 1)
+    );
   }
-  else if (ch === '/')
+
+  if (ch === '/')
   {
     if (dir.r !== 0)
     {
-      if (dir.r > 0)
-      {
-        debug('reflect down to left');
-        walk(grid, npos, { r: 0, c: -1 }, visited, beam);
-      }
-      else
-      {
-        debug('reflect up to right');
-        walk(grid, npos, { r: 0, c: 1 }, visited, beam);
-      }
+      return walk(grid, npos.r, npos.c, { r: 0, c: dir.r > 0 ? -1 : 1 },
+        visited, m, beam);
     }
-    else
-    {
-      if (dir.c > 0)
-      {
-        debug('reflect right to up');
-        walk(grid, npos, { r: -1, c: 0 }, visited, beam);
-      }
-      else
-      {
-        debug('reflect left to down');
-        walk(grid, npos, { r: 1, c: 0 }, visited, beam);
-      }
-    }
+    return walk(grid, npos.r, npos.c, { r: dir.c > 0 ? -1 : 1, c: 0 },
+      visited, m, beam);
   }
-  else if (ch === '\\')
+
+  if (ch === '\\')
   {
     if (dir.r !== 0)
     {
-      if (dir.r > 0)
-      {
-        debug('reflect down to right');
-        walk(grid, npos, { r: 0, c: 1 }, visited, beam);
-      }
-      else
-      {
-        debug('reflect up to left');
-        walk(grid, npos, { r: 0, c: -1 }, visited, beam);
-      }
+      return walk(grid, npos.r, npos.c, { r: 0, c: dir.r > 0 ? 1 : -1 },
+        visited, m, beam);
     }
-    else
-    {
-      if (dir.c > 0)
-      {
-        debug('reflect right to down');
-        walk(grid, npos, { r: 1, c: 0 }, visited, beam);
-      }
-      else
-      {
-        debug('reflect left to up');
-        walk(grid, npos, { r: -1, c: 0 }, visited, beam);
-      }
-    }
+    return walk(grid, npos.r, npos.c, { r: dir.c > 0 ? 1 : -1, c: 0 },
+      visited, m, beam);
   }
-  else
-  {
-    throw new Error(`unknown character found on grid: '${ch}' at r = ${npos.r}, c = ${npos.c}`);
-  }
+
+  throw new Error(`unknown character found on grid: '${ch}' at r = ${npos.r}, c = ${npos.c}`);
 }
 
 function solve1(data)
 {
-  const visited = new Set();
-  walk(data, { r: 0, c: -1 }, { r: 0, c: 1 }, visited);
-
-  const unique = new Set();
-  for (const v of visited)
-  {
-    unique.add(v.split(':')[0]);
-  }
-
-  debug(unique);
-
-  return unique.size;
+  return walk(data, 0, -1, { r: 0, c: 1 });
 }
 
-function solve2()
+function solve2(data)
 {
-  return 'todo';
+  const scores = [];
+  for (let r = 0; r < data.length; r++)
+  {
+    scores.push(walk(data, r, -1, { r: 0, c: 1 }));
+    scores.push(walk(data, r, data[0].length, { r: 0, c: -1 }));
+  }
+  for (let c = 0; c < data[0].length; c++)
+  {
+    scores.push(walk(data, -1, c, { r: 1, c: 0 }));
+    scores.push(walk(data, data.length, c, { r: -1, c: 0 }));
+  }
+
+  return Math.max(...scores);
 }
 
 export default async function day16(target)
@@ -176,16 +140,20 @@ export default async function day16(target)
 
   const part1 = solve1(data);
   const expect1 = 46;
-  if (target.includes('example1') && part1 !== expect1)
+  if (target.includes('example') && part1 !== expect1)
   {
     throw new Error(`Invalid part 1 solution: ${part1}. Expecting; ${expect1}`);
   }
 
   const part2 = solve2(data);
-  const expect2 = 'todo';
-  if (target.includes('example2') && part2 !== expect2)
+  const expect2 = 51;
+  if (target.includes('example') && part2 !== expect2)
   {
     throw new Error(`Invalid part 2 solution: ${part2}. Expecting; ${expect2}`);
+  }
+  if (target.includes('data.txt') && part2 !== 8244)
+  {
+    throw new Error(`Invalid part 2 solution: ${part2}. Expecting; 8244`);
   }
 
   return { day: 'day16', part1, part2, duration: Date.now() - start };
