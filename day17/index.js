065d4aea2875d93bd4721a0ec5649258c92c397e
Adjel van der Meer <eavdmeer@gmail.com>
2023-12-17 14:26:53 +0100

    Add day 17 part 2 solution

--- day17/index.js
+++ day17/index.js
@@ -9,28 +9,35 @@ if (process.argv[2])
   day17(process.argv[2]).then(console.log);
 }
 
-function neighbors(data, r, c, d, steps)
+function neighbors(data, r, c, d, steps, part)
 {
-  const allowRepeat = steps < 3;
   const back = { U: 'D', D: 'U', R: 'L', L: 'R' };
   const points = [
     [ r, c + 1, 'R' ],
     [ r + 1, c, 'D' ],
     [ r, c - 1, 'L' ],
     [ r - 1, c, 'U' ]
-  ];
-  return points
-    .filter(([ rp, cp, dp ]) =>
-      rp >= 0 &&
-      rp < data.length &&
-      cp >= 0 &&
-      cp < data[0].length &&
-      dp !== back[d] &&
-      (allowRepeat || dp !== d)
-    );
+  ].filter(([ rp, cp, dp ]) =>
+    rp >= 0 && rp < data.length &&
+    cp >= 0 && cp < data[0].length &&
+    dp !== back[d]);
+
+  if (part === 1)
+  {
+    const allowRepeat = steps < 3;
+    return points.filter(([ , , dp ]) => allowRepeat || dp !== d);
+  }
+  if (steps > 0 && steps < 4)
+  {
+    // Only allow same direction
+    return points.filter(([ , , dp ]) => dp === d);
+  }
+
+  const allowRepeat = steps < 10;
+  return points.filter(([ , , dp ]) => allowRepeat || dp !== d);
 }
 
-function solve(data, rs, cs, re, ce)
+function solve(data, rs, cs, re, ce, part = 1)
 {
   const heap = new MinPriorityQueue(v => v.cost);
 
@@ -48,10 +55,16 @@ function solve(data, rs, cs, re, ce)
     path.push({ r, c, d, cost });
     seen.set(key, { r, c, d, steps, cost });
 
-    if (r === re && c === ce) { return { path, cost }; }
+    if (r === re && c === ce)
+    {
+      if (part === 1 || steps >= 4) { return { path, cost }; }
+      continue;
+    }
 
     // Add all neighbors we are allowed to move to on the heap
-    neighbors(data, r, c, d, steps).forEach(([ lr, lc, ld ]) =>
+    const next = neighbors(data, r, c, d, steps, part);
+
+    next.forEach(([ lr, lc, ld ]) =>
     {
       heap.push({
         cost: cost + data[lr][lc],
@@ -73,17 +86,29 @@ function solve1(data)
       )
     );
 
-  // return solve(grid, 0, 0, data.length - 1, data[0].length - 1);
-  const solution = solve(grid, 0, 0, data.length - 1, data[0].length - 1);
-  console.log('final path:', solution.path.map(v => v.d).join(''),
+  const solution = solve(grid, 0, 0, data.length - 1, data[0].length - 1, 1);
+
+  debug('final path:', solution.path.map(v => v.d).join(''),
     'with cost:', solution.cost);
 
   return solution.cost;
 }
 
-function solve2()
+function solve2(data)
 {
-  return 'todo';
+  const grid = data
+    .map(line => line
+      .split('')
+      .map(v => parseInt(v, 10)
+      )
+    );
+
+  const solution = solve(grid, 0, 0, data.length - 1, data[0].length - 1, 2);
+
+  debug('final path:', solution.path.map(v => v.d).join(''),
+    'with cost:', solution.cost);
+
+  return solution.cost;
 }
 
 export default async function day17(target)
@@ -105,7 +130,7 @@ export default async function day17(target)
 
   const part1 = solve1(data);
   const expect1 = 102;
-  if (target.includes('example1') && part1 !== expect1)
+  if (target.includes('example') && part1 !== expect1)
   {
     throw new Error(`Invalid part 1 solution: ${part1}. Expecting; ${expect1}`);
   }
@@ -115,8 +140,8 @@ export default async function day17(target)
   }
 
   const part2 = solve2(data);
-  const expect2 = 'todo';
-  if (target.includes('example2') && part2 !== expect2)
+  const expect2 = 94;
+  if (target.includes('example') && part2 !== expect2)
   {
     throw new Error(`Invalid part 2 solution: ${part2}. Expecting; ${expect2}`);
   }
