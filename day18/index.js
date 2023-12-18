4c917c2e3997125c91396320bd4447bbf14f24a9
Adjel van der Meer <eavdmeer@gmail.com>
2023-12-18 21:19:36 +0100

    Explain code for day 18

--- day18/index.js
+++ day18/index.js
@@ -8,6 +8,30 @@ if (process.argv[2])
   day18(process.argv[2]).then(console.log);
 }
 
+/*
+ * Use Shoelace formula to get the inner surface of an N-point polygon:
+ *
+ * https://en.wikipedia.org/wiki/Shoelace_formula
+ *
+ * A = Math.abs( sum(y[i] * (x[i - 1] - x[i + 1]) / 2 )  for i in 1..N
+ *
+ * This, however, calculates the inner surface from the middle of each
+ * 'pixel', so it does not count the boundary pixels correctly.
+ *
+ * Now combine this with Pick's theorem to calculate the number of interior
+ * points I for a polygon with area A and B integer boundary points:
+ *
+ * A = I + B / 2 - 1;
+ *
+ * so:
+ *
+ * I = A + 1 - B / 2
+ *
+ * The total T we are looking for is I + B, so
+ *
+ * T = a + 1 + B / 2
+ */
+
 function dig(ops)
 {
   let r = 0;
@@ -37,7 +61,7 @@ function dig(ops)
 
   const area = Math.abs(points
     .map((p, i) =>
-      p[0] * (points.at(i - 1).at(1) - points.at(modn(i + 1)).at(1)))
+      p[1] * (points.at(i - 1).at(0) - points.at(modn(i + 1)).at(0)))
     .reduce((a, v) => a + v, 0)) / 2;
 
   debug('area:', area);
@@ -92,12 +116,20 @@ export default async function day18(target)
   {
     throw new Error(`Invalid part 1 solution: ${part1}. Expecting; ${expect1}`);
   }
+  if (target.includes('data.txt') && part1 !== 52035)
+  {
+    throw new Error(`Invalid part 1 solution: ${part1}. Expecting; 52035`);
+  }
 
   const part2 = solve2(data);
   const expect2 = 952408144115;
   if (target.includes('example2') && part2 !== expect2)
   {
     throw new Error(`Invalid part 2 solution: ${part2}. Expecting; ${expect2}`);
+  }
+  if (target.includes('data.txt') && part2 !== 60612092439765)
+  {
+    throw new Error(`Invalid part 2 solution: ${part2}. Expecting; 60612092439765`);
   }
 
   return { day: 'day18', part1, part2, duration: Date.now() - start };
